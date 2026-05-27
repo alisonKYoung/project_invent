7237bb607934d36c916c9c433e302133721d44b0
alisonKYoung <ayoungperson1@gmail.com>
2026-05-27 08:55:06 -0700

    change to 200 shift

--- webserial.js
+++ webserial.js
@@ -155,7 +155,7 @@ async function disconnect() {
 function updateGraph(x, y) {
     gasChart.data.labels.push(x);
     gasChart.data.datasets[0].data.push(y);
-    if (gasChart.data.labels.length > 500) {
+    if (gasChart.data.labels.length > 200) {
         gasChart.data.labels.shift();
         gasChart.data.datasets[0].data.shift();
     }
